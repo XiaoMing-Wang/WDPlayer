c852a19dcf37be8b7328c72c0eec0122e25f9a28
wq <wq>
2021-02-05 15:31:43 +0800

    no message

---
+++ WDPlayerAssistant.swift
@@ -0,0 +1,65 @@
+//
+//  WDPlayerAssistant.swift
+//  Multi-Project-Swift
+//
+//  Created by imMac on 2021/2/4.
+//
+
+import UIKit
+
+class WDPlayerAssistant: NSObject {
+
+    /**< 实例化手势 */
+    @discardableResult
+    class func addTapGesture(_ target: UIView, taps: Int, touches: Int, selector: Selector) -> UITapGestureRecognizer {
+        let gesture = UITapGestureRecognizer(target: target, action: selector)
+        gesture.numberOfTapsRequired = taps
+        gesture.numberOfTouchesRequired = touches
+        target.isUserInteractionEnabled = true
+        target.addGestureRecognizer(gesture)
+        return gesture
+    }
+
+    /// 时间转化
+    /// - Parameter duration: duration
+    /// - Returns: 时间
+    class func timeTranslate(_ duration: Int) -> String {
+        let durationSeconds = lroundf(Float(duration))
+        let minutes = durationSeconds / 60
+        let seconds = durationSeconds % 60
+        if durationSeconds >= 3600 {
+            let hours = durationSeconds / 3600
+            return String(format: "%02zd:%02zd:%02zd", hours, minutes, seconds)
+        } else {
+            return String(format: "%02zd:%02zd", minutes, seconds)
+        }
+    }
+
+    class func locationWindow_play(_ view: UIView) -> CGRect {
+        guard let window = UIApplication.shared.keyWindow else { return .zero }
+        return view.convert(view.bounds, to: window)
+    }
+    
+    /**< 截图 */
+    class func makeImageWindow_play(_ view: UIView?) -> UIImageView? {
+        guard let view = view else { return nil }
+        let width: CGFloat = UIScreen.main.bounds.size.width
+        let barHeight: CGFloat = (44 + barHeight_play())
+        let imageView = UIImageView()
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: width, height: barHeight), false, UIScreen.main.scale)
+        view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext();
+        imageView.frame = CGRect(x: 0, y: 0, width: width, height: barHeight)
+        UIGraphicsEndImageContext();
+        return imageView
+    }
+
+    /**< 状态栏高度 */
+    fileprivate class func barHeight_play() -> CGFloat {
+        if #available(iOS 13, *) {
+            return UIApplication.shared.windows.first?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
+        } else {
+            return UIApplication.shared.statusBarFrame.height
+        }
+    }
+}
---
+++ view/WDPlayerTouchView.swift
@@ -0,0 +1,121 @@
+//
+//  WDPlayerTouchView.swift
+//  Multi-Project-Swift
+//
+//  Created by imMac on 2021/2/5.
+//
+
+import UIKit
+
+protocol WDPlayerTouchViewDelegate: class {
+
+    /**< 单击 */
+    func singleTap(touchView: WDPlayerTouchView)
+
+    /**< 双击 */
+    func doubleTap(touchView: WDPlayerTouchView)
+
+    /**< 开始 */
+    func resumePlay(touchView: WDPlayerTouchView)
+
+}
+
+extension WDPlayerTouchView {
+
+    /// 显示菊花
+    func showLoadingView(afterDelay: TimeInterval = 0.5) {
+        NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(_showLoadingView), object: nil)
+        perform(#selector(_showLoadingView), with: nil, afterDelay: afterDelay)
+    }
+
+    /// 隐藏菊花
+    func hiddenLoadingView() {
+        NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(_showLoadingView), object: nil)
+        loadingView.hide()
+        loadingView.isHidden = true
+    }
+
+}
+
+class WDPlayerTouchView: UIView {
+
+    fileprivate weak var delegate: WDPlayerTouchViewDelegate? = nil
+    public var isSuspended: Bool = false {
+        didSet {
+            suspendButton.isHidden = !isSuspended
+            if suspendButton.isHidden == false {
+                hiddenLoadingView()
+            }
+        }
+    }
+
+    convenience init(delegate: WDPlayerTouchViewDelegate?) {
+        self.init()
+        self.delegate = delegate
+        self.addGestures()
+        self.addLoadingView()
+        self.hiddenLoadingView()
+    }
+
+    @objc fileprivate func _showLoadingView() {
+        kLogPrint(2222222)
+        loadingView.start()
+        loadingView.isHidden = false
+    }
+
+    @objc func singleTap() {
+        delegate?.singleTap(touchView: self)
+    }
+
+    @objc func doubleTap() {
+        delegate?.doubleTap(touchView: self)
+    }
+
+    @objc func suspend() {
+        delegate?.resumePlay(touchView: self)
+    }
+
+    fileprivate func addLoadingView() {
+        addSubview(loadingView)
+        addSubview(suspendButton)
+        automaticLayout()
+    }
+
+    fileprivate func automaticLayout() {
+        let width = loadingView.frame.size.width
+        loadingView.snp.makeConstraints { (make) in
+            make.width.height.equalTo(width)
+            make.center.equalToSuperview()
+        }
+
+        suspendButton.snp.makeConstraints { (make) in
+            make.width.height.equalTo(52)
+            make.center.equalToSuperview()
+        }
+    }
+    
+    /**< 添加手势 */
+    fileprivate func addGestures() {
+        if WDPlayConf.supportDoubleClick {
+            let singleGesture = WDPlayerAssistant.addTapGesture(self, taps: 1, touches: 1, selector: #selector(singleTap))
+            let doubleGesture = WDPlayerAssistant.addTapGesture(self, taps: 2, touches: 1, selector: #selector(doubleTap))
+            singleGesture.require(toFail: doubleGesture)
+        } else {
+            WDPlayerAssistant.addTapGesture(self, taps: 1, touches: 1, selector: #selector(doubleTap))
+        }
+    }
+
+    fileprivate lazy var loadingView: WDPLayLoadingView = {
+        var loadingView = WDPLayLoadingView(frame: CGRect(x: 0, y: 0, width: 35, height: 35))
+        return loadingView
+    }()
+    
+    /**< 暂停图标 */
+    fileprivate lazy var suspendButton: UIButton = {
+        var suspendButton = UIButton()
+        suspendButton.setImage(UIImage(named: "new_allPlay_44x44_"), for: .normal)
+        suspendButton.isHidden = true
+        suspendButton.addTarget(self, action: #selector(suspend), for: .touchUpInside)
+        return suspendButton
+    }()
+}
