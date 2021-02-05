bc68ebf1a3f7f2636a08a5f4229d504057d04fd7
wq <wq>
2021-02-05 15:30:00 +0800

    no message

---
+++ WDPlayerLayerView.swift
@@ -0,0 +1,297 @@
+//
+//  WDPlayerLayerView.swift
+//  Multi-Project-Swift
+//
+//  Created by imMac on 2021/2/4.
+//
+
+import UIKit
+import SnapKit
+import AVFoundation
+
+protocol WDPlayerLayerViewDelegate: class {
+
+    /**< 暂停 */
+    func suspended(layerView: WDPlayerLayerView)
+
+    /**< 继续播放 */
+    func play(layerView: WDPlayerLayerView)
+
+    /**< 进度 */
+    func eventValueChanged(currentlTime: Int)
+}
+
+/**< 播放界面 */
+class WDPlayerContentView: UIImageView {
+
+    override class var layerClass: AnyClass {
+        return AVPlayerLayer.self
+    }
+
+    var playerLayer: AVPlayerLayer? {
+        return layer as? AVPlayerLayer
+    }
+
+    func setPlayer(_ avplayer: AVPlayer?) {
+        playerLayer?.player = avplayer
+    }
+}
+
+extension WDPlayerLayerView {
+
+    /// 设置拉伸类型
+    /// - Parameter contentMode: contentMode
+    func setContentMode(_ contentMode: WDPlayConf.ContentMode) {
+        _setContentMode(contentMode)
+    }
+
+    /// 设置总时间
+    /// - Parameter duration: duration
+    func setTotalDuration(_ duration: Int) {
+        toolbarView.totalTime = duration
+    }
+
+    /// 设置当前时间
+    /// - Parameter duration: duration
+    func setCurrentDuration(_ duration: Int) {
+        toolbarView.currentlTime = duration
+    }
+
+    /// 显示隐藏菊花
+    /// - Parameter display: display
+    func disPlayLoadingView(_ display: Bool = true, afterDelay: TimeInterval = 0.5) {
+        display ? touchView.showLoadingView(afterDelay: afterDelay) : touchView.hiddenLoadingView()
+    }
+
+}
+
+fileprivate extension WDPlayerLayerView {
+    
+    func _setContentMode(_ contentMode: WDPlayConf.ContentMode) {
+        contentModeType = contentMode
+
+        /**< 有黑边的那种 */
+        if contentMode == .blackBorder {
+            contentsView.contentMode = .scaleAspectFit
+            contentsView.playerLayer?.videoGravity = .resizeAspect
+
+        /**< 填充满变形 */
+        } else if contentMode == .fullScreenVariant {
+            contentsView.contentMode = .scaleToFill
+            contentsView.playerLayer?.videoGravity = .resize
+
+        /**< 填充满不变形 */
+        } else if contentMode == .fullScreenTailor {
+            contentsView.contentMode = .scaleAspectFill
+            contentsView.playerLayer?.videoGravity = .resizeAspectFill
+        }
+    }
+    
+//    /**< 暂停动画 */
+//    func pauseAnimation() {
+//        isAnimation = true
+//        suspendButton.alpha = 1
+//        suspendButton.isHidden = false
+//        isSuspended ? (suspendButton.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)) : ()
+//        UIView.animate(withDuration: 0.0) {
+//            self.isSuspended ? (self.suspendButton.transform = .identity) : (self.suspendButton.alpha = 0)
+//        } completion: { _ in
+//            self.isAnimation = false
+//        }
+//    }
+    
+    /**< 设置layer */
+    func setPlaybackLayer(player: AVPlayer?) {
+        contentsView.setPlayer(player)
+    }
+
+    /**< 暂停 */
+    @objc func suspend(transform: Bool = true) {
+        guard isAnimation == false else { return }
+        if transform { isSuspended = !isSuspended }
+        isSuspended ? delegate?.suspended(layerView: self) : delegate?.play(layerView: self)
+        touchView.isSuspended = isSuspended
+        toolbarView.isSuspended = isSuspended
+        cancelHideToolbar()
+    }
+
+    /**< 双击 */
+    @objc func showToolBar() {
+        NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(hiddenToolBar), object: nil)
+        if isShowToolBar {
+            topBar.isHidden = true
+            toolbarView.isHidden = true
+        } else {
+            topBar.isHidden = false
+            toolbarView.isHidden = false
+            perform(#selector(hiddenToolBar), with: nil, afterDelay: 3)
+        }
+        isShowToolBar = !toolbarView.isHidden
+    }
+
+    @objc func hiddenToolBar() {
+        UIView.animate(withDuration: 0.25) {
+            self.topBar.alpha = 0
+            self.toolbarView.alpha = 0
+        } completion: { _ in
+            self.topBar.alpha = 1
+            self.toolbarView.alpha = 1
+            self.topBar.isHidden = true
+            self.toolbarView.isHidden = true
+            self.isShowToolBar = false
+        }
+    }
+
+}
+
+/**< 工具栏回调  触摸view回调 */
+extension WDPlayerLayerView: WPPlayerViewBarDelegate, WDPlayerTouchViewDelegate {
+
+    /**< 单击 */
+    func singleTap(touchView: WDPlayerTouchView) {
+        if WDPlayConf.supportDoubleClick {
+            showToolBar()
+        } else {
+            suspend()
+        }
+    }
+
+    /**< 双击 */
+    func doubleTap(touchView: WDPlayerTouchView) {
+        suspend()
+    }
+
+    /**< 恢复播放 */
+    func resumePlay(touchView: WDPlayerTouchView) {
+        isSuspended = true
+        suspend()
+    }
+
+    /**< 进度滑动 */
+    func eventValueChanged(currentlTime: Int) {
+        delegate?.eventValueChanged(currentlTime: currentlTime)
+    }
+
+    /**< 暂停播放 */
+    func suspended(isSuspended: Bool) {
+        self.isSuspended = isSuspended
+        self.suspend(transform: false)
+    }
+
+    /**< 取消消失工具栏 */
+    func cancelHideToolbar() {
+        NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(hiddenToolBar), object: nil)
+        perform(#selector(hiddenToolBar), with: nil, afterDelay: 3)
+    }
+
+    /**< 返回按钮点击 */
+    func backClick() {
+
+    }
+    
+    /// 点击全屏按钮
+    func fullClick(isFull: Bool) {
+        isFull ? full() : thum()
+    }
+    
+    fileprivate func full() {
+        originalSizePlay = frame.size
+        originalCenterYPlay = WDPlayerAssistant.locationWindow_play(self).origin.y + (originalSizePlay.height / 2)
+        cancelHideToolbar()
+
+        let rootViewController = UIApplication.shared.keyWindow?.rootViewController
+        let fullViewController = WDPlayerFullViewController()
+        fullViewController.modalPresentationStyle = .fullScreen
+        self.fullViewController = fullViewController
+        if let imaView = WDPlayerAssistant.makeImageWindow_play(rootViewController?.view) {
+            rootViewController?.view.addSubview(imaView)
+            rootViewController?.present(fullViewController, animated: true, completion: {
+                imaView.removeFromSuperview()
+            })
+        }
+    }
+
+    fileprivate func thum() {
+        self.fullViewController?.dismiss(animated: true, completion: nil)
+    }
+}
+
+class WDPlayerLayerView: UIView {
+
+    fileprivate weak var delegate: WDPlayerLayerViewDelegate? = nil
+    fileprivate var isSuspended: Bool = false
+    fileprivate var isAnimation: Bool = false
+    fileprivate var isShowToolBar: Bool = true
+    fileprivate var contentModeType: WDPlayConf.ContentMode? = nil
+    fileprivate weak var fullViewController: WDPlayerFullViewController? = nil
+ 
+    convenience init(player: AVPlayer?, delegate: WDPlayerLayerViewDelegate?) {
+        self.init(frame: .zero)
+        self.delegate = delegate
+        initializationInterface()
+        setPlaybackLayer(player: player)
+    }
+    
+    fileprivate func initializationInterface() {
+        tag = WDPlayConf.playerLayerTag
+        backgroundColor = .black
+        clipsToBounds = true
+
+        addSubview(contentsView)
+        addSubview(touchView)
+        WDPlayConf.showTopBar ? addSubview(topBar) : ()
+        WDPlayConf.showToolBar ? addSubview(toolbarView) : ()
+
+        automaticLayout()
+        setContentMode(.blackBorder)
+    }
+
+    fileprivate func automaticLayout() {
+        contentsView.snp.makeConstraints { (make) in
+            make.edges.equalTo(0)
+        }
+        
+        touchView.snp.makeConstraints { (make) in
+            make.left.right.top.bottom.equalTo(0)
+        }
+        
+        if WDPlayConf.showTopBar {
+            topBar.snp.makeConstraints { (make) in
+                make.left.right.top.equalTo(0)
+                make.height.equalTo(WDPlayConf.toolBarHeight)
+            }
+        }
+
+        if WDPlayConf.showToolBar {
+            toolbarView.snp.makeConstraints { (make) in
+                make.left.right.bottom.equalTo(0)
+                make.height.equalTo(WDPlayConf.toolBarHeight)
+            }
+        }
+    }
+           
+    /**< 播放界面 */
+    fileprivate lazy var contentsView: WDPlayerContentView = {
+        var contentsView = WDPlayerContentView()
+        return contentsView
+    }()
+    
+    /**< 顶部工具栏 */
+    fileprivate lazy var topBar: WPPlayerViewBar = {
+        var topBar = WPPlayerViewBar(titles: "", delegate: self)
+        return topBar
+    }()
+
+    /**< 中间的触摸view */
+    fileprivate lazy var touchView: WDPlayerTouchView = {
+        var touchView = WDPlayerTouchView(delegate: self)
+        return touchView
+    }()
+
+    /**< 底部工具栏 */
+    fileprivate lazy var toolbarView: WPPlayerViewToolbar = {
+        var toolbarView = WPPlayerViewToolbar(totalTime: 0, delegate: self)
+        return toolbarView
+    }()
+         
+}
---
+++ WDPlayerSession.swift
@@ -0,0 +1,353 @@
+//
+//  WPPlayer.swift
+//  Multi-Project-Swift
+//
+//  Created by imMac on 2021/2/4.
+//
+
+import UIKit
+import AVFoundation
+import KTVHTTPCache
+
+extension WDPlayerSession {
+    
+    /**< 播放 */
+    func play() {
+
+        /**< 用户手动暂停的 不允许播放 */
+        if status == .initiative { return }
+        if status == .wait {
+            backBagin(mandatory: true)
+        }
+
+        player?.play()
+        status = .play
+        stopBuffer = false
+        playerItem?.preferredForwardBufferDuration = 0
+        playerItem?.preferredPeakBitRate = TimeInterval(100000)
+        
+        /**< 当前播放的资源 */
+        WDPlayConf.currentPlayURL = playURL
+    }
+    
+    /**< 暂停 */
+    func pause() {
+
+        /**< 用户手动暂停的  */
+        if status == .initiative { return }
+        player?.pause()
+        status = .pause
+    }
+    
+    /**< 把进度设置到最后 */
+    func stop() {
+        status = .wait
+        stopBuffer = true
+        playerItem?.preferredForwardBufferDuration = TimeInterval(0.1)
+        playerItem?.preferredPeakBitRate = 1
+        playerItem?.seek(to: CMTimeMakeWithSeconds(100000, preferredTimescale: 1))
+        playerItem?.canUseNetworkResourcesForLiveStreamingWhilePaused = false
+        playerItem?.cancelPendingSeeks()
+        playerItem?.asset.cancelLoading()
+        player?.seek(to: CMTimeMakeWithSeconds(100000, preferredTimescale: 1))
+        player?.pause()
+    }
+    
+    /**< 回到开头 */
+    func backBagin(mandatory: Bool = false) {
+        if status == .play, mandatory == false { return }
+        playerItem?.seek(to: .zero)
+        player?.seek(to: .zero)
+    }
+    
+    /**< 切换 */
+    func replacePlayURL(_ nextURL: String?) {
+        guard let nextURL = nextURL else {
+            destruction()
+            return
+        }
+
+        playURL = nextURL
+        replaceItem()
+    }
+
+    /**< 释放 */
+    func destruction(removeSuperview: Bool = true) {
+        if status == .background { return }
+        if playerItem == nil, player == nil {
+            return
+        }
+        
+        player?.pause()
+        do {
+            playerItem?.removeObserver(self, forKeyPath: "status")
+            playerItem?.removeObserver(self, forKeyPath: "playbackBufferEmpty")
+            playerItem?.removeObserver(self, forKeyPath: "playbackLikelyToKeepUp")
+            playerItem?.removeObserver(self, forKeyPath: "playbackBufferFull")
+            player?.removeObserver(self, forKeyPath: "timeControlStatus")
+        } catch { }
+
+        if let observerAny = self.observerAny {
+            player?.removeTimeObserver(observerAny)
+            self.observerAny = nil
+        }
+
+        status = .destruction
+        playerLayer?.removeFromSuperlayer()
+        playURL = nil
+        proxyUrl = nil
+        playerItem = nil
+        playerLayer = nil
+        player?.replaceCurrentItem(with: nil)
+        player = nil
+        NotificationCenter.default.removeObserver(self)
+        if WDPlayConf.currentPlayURL == playURL, playURL != nil {
+            WDPlayConf.currentPlayURL = nil
+        }
+    }
+        
+}
+
+
+class WDPlayerSession: NSObject {
+    
+    enum Status {
+        case unknow      /**< 未知 */
+        case buffer      /**< 缓冲 */
+        case wait        /**< 等待播放 */
+        case pause       /**< 系统暂停 */
+        case initiative  /**< 手动暂停 */
+        case play        /**< 播放 */
+        case fail        /**< 失败 */
+        case background  /**< 进入后台 */
+        case destruction /**< 释放无任务 */
+    }
+
+    /**< 播放view */
+    public var playView: WDPlayerLayerView = WDPlayerLayerView()
+
+    /**< 封面url */
+    public var coverURL: URL? = nil
+
+    /**< 自动播放 */
+    public var isAutoPlay: Bool = true
+
+    /**< 是否重播 */
+    public var isReplay: Bool = true
+
+    /**< 总时长 */
+    fileprivate(set) var duration: Int = -1
+    fileprivate(set) var currentDuration: Int = 0
+
+    /**< 播放状态 */
+    fileprivate(set) var status: Status = .unknow
+
+    /**< 是否停止缓冲 */
+    fileprivate var stopBuffer: Bool = false
+
+    /**< 是否可以直接播放(缓冲足够) */
+    fileprivate(set) var isToKeepUp: Bool = false
+    fileprivate(set) var isLoadTimeControlStatus: Bool = false
+
+    /**< 播放资源 */
+    fileprivate(set) var playURL: String? = nil
+    fileprivate(set) var proxyUrl: URL? = nil
+
+    /**< 失败重试次数 */
+    fileprivate var retryCount: Int = 0
+    fileprivate var player: AVPlayer? = nil
+    fileprivate var playerLayer: AVPlayerLayer? = nil
+    fileprivate var playerItem: AVPlayerItem? = nil
+    fileprivate var observerAny: Any? = nil
+    
+    convenience init(playURL: String, stopBuffer: Bool = false) {
+        self.init()
+        self.stopBuffer = stopBuffer
+        self.playURL = playURL
+        self.replaceItem()
+        self.listenNotification()
+    }
+
+    /**< 切换item 网络路径转化成本地路径 */
+    fileprivate func replaceItem() {
+        destruction()
+        
+        if let playURL = playURL, let url = URL(string: playURL) {
+            if let proxyUrl = KTVHTTPCache.proxyURL(withOriginalURL: url) {
+                self.proxyUrl = proxyUrl
+                playerItem = AVPlayerItem(url: proxyUrl)
+                player = AVPlayer(playerItem: playerItem)
+                playerLayer = AVPlayerLayer(player: player)
+                playView = WDPlayerLayerView(player: player, delegate: self)
+                observerAny = player?.addPeriodicTimeObserver(forInterval: CMTime(value: 1, timescale: 1), queue: .global(), using: { /* [weak self] */progressTime in
+                    let currentDuration = lroundf(Float(CMTimeGetSeconds(progressTime)))
+                    self.currentDuration = currentDuration
+                    DispatchQueue.main.async {
+                        self.playView.setCurrentDuration(currentDuration)
+                    }
+                })
+                
+                /**< 获取时长 */
+                DispatchQueue.global().async {
+                    if let assetDuration = self.playerItem?.asset.duration {
+                        let duration = lroundf(Float(CMTimeGetSeconds(assetDuration)))
+                        self.duration = duration
+                        DispatchQueue.main.async {
+                            self.playView.setTotalDuration(duration)
+                        }
+                    }
+                }
+                                            
+                status = .buffer
+                isToKeepUp = false
+                playerItem?.addObserver(self, forKeyPath: "status", options: .new, context: nil)
+                playerItem?.addObserver(self, forKeyPath: "playbackBufferEmpty", options: .new, context: nil)
+                playerItem?.addObserver(self, forKeyPath: "playbackLikelyToKeepUp", options: .new, context: nil)
+                playerItem?.addObserver(self, forKeyPath: "playbackBufferFull", options: .new, context: nil)
+                player?.addObserver(self, forKeyPath: "timeControlStatus", options: .new, context: nil)
+                if stopBuffer {
+                    stop()
+                }
+            }
+        }
+    }
+    
+    /**< 获取时长 */
+    func getAssetDuration() {
+        guard duration == -1 else { return }
+        DispatchQueue.global().async {
+            if let assetDuration = self.playerItem?.asset.duration {
+                let duration = lroundf(Float(CMTimeGetSeconds(assetDuration)))
+                self.duration = duration
+                DispatchQueue.main.async {
+                    self.playView.setTotalDuration(duration)
+                }
+            }
+        }
+    }
+    
+    /**< 监听回调 */
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+
+        /**< 缓冲区域 */
+        if (keyPath == "loadedTimeRanges") {
+                        
+        }
+
+        /**< 播放状态 ios10+ */
+        if (keyPath == "timeControlStatus") {
+            if player?.timeControlStatus == .waitingToPlayAtSpecifiedRate {
+                playView.disPlayLoadingView(true)
+            } else {
+                playView.disPlayLoadingView(false)
+            }
+            isLoadTimeControlStatus = true
+        }
+
+        /**< 缓冲不足播放 */
+        if (keyPath == "playbackBufferEmpty") {
+            if playerItem?.isPlaybackBufferEmpty == true {
+                isToKeepUp = false
+            }
+        }
+
+        /**< 缓冲可以播放 */
+        if (keyPath == "playbackLikelyToKeepUp") {
+            if playerItem?.isPlaybackLikelyToKeepUp == true {
+                isToKeepUp = true
+            }
+        }
+        
+        /**< 状态 */
+        if (keyPath == "status") {
+            if playerItem?.status == .readyToPlay, status == .play || isAutoPlay {
+                play()
+                getAssetDuration()
+                
+            } else if playerItem?.status == .readyToPlay, status == .buffer || status == .wait {
+                stop()
+                getAssetDuration()
+                
+            } else if playerItem?.status == .failed || playerItem?.status == .unknown {
+                status = .fail
+                kLogPrint("视频缓冲失败 :\(playURL!)")
+                kLogPrint("视频缓冲失败 :\(playerItem!.error.debugDescription)")
+                if (playerItem?.error as NSError?)?.code == -11829, retryCount == 0 {
+                    kLogPrint("================================")
+                }
+            }
+            
+        }
+
+    }
+
+    deinit {
+        status = .pause
+        destruction()
+    }
+    
+    /**< 通知 */
+    fileprivate func listenNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(playerItemDidReachEnd(notification:)), name: .AVPlayerItemDidPlayToEndTime, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(resignActive), name: UIApplication.willResignActiveNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(becomeActive), name: UIApplication.didBecomeActiveNotification, object: nil)
+    }
+
+    /**< 播放结束 */
+    @objc fileprivate func playerItemDidReachEnd(notification: Notification) {
+        backBagin(mandatory: true)
+        if isReplay, status == .play {
+            play()
+
+        } else if isReplay == false {
+
+            stop()
+        }
+    }
+
+    /**< 后台 */
+    @objc fileprivate func resignActive() {
+        if status == .play {
+            pause()
+            status = .background
+        }
+    }
+    
+    /**< 前台 */
+    @objc fileprivate func becomeActive() {
+        if status == .background {
+            play()
+        }
+    }
+}
+
+extension WDPlayerSession: WDPlayerLayerViewDelegate {
+
+    /// 暂停回调
+    /// - Parameter layerView: layerView
+    func suspended(layerView: WDPlayerLayerView) {
+        status = .initiative
+        player?.pause()
+    }
+
+    /// 播放回调
+    /// - Parameter layerView: layerView
+    func play(layerView: WDPlayerLayerView) {
+        status = .play
+        player?.play()
+    }
+
+    /// 进度
+    /// - Parameters:
+    ///   - layerView: layerView
+    ///   - currentlTime: currentlTime
+    func eventValueChanged(currentlTime: Int) {
+        player?.pause()
+        player?.seek(to: CMTimeMakeWithSeconds(Float64(currentlTime), preferredTimescale: 1), toleranceBefore: .zero, toleranceAfter: .zero)
+        if status == .play {
+            player?.play()
+        }
+    }
+
+    
+}
